81e8e7e3579328f3633dc5f3e0cdea54b03e7aeb
Dusttvin <kunal0519.becse24@chitkara.edu.in>
2025-02-04 14:37:36 +0530

    Solution to Logical NOT and AND Combination

--- Logical NOT and AND Combination@@67882a731b4392d8fc1577e0/code.c
+++ Logical NOT and AND Combination@@67882a731b4392d8fc1577e0/code.c
@@ -1,16 +1,14 @@
-Logical NOT and AND Combination 
-Write a C program to check if neither of the two numbers is greater than zero using `!` and `&& operators. 
-Input Format 
-Two space-separated integers. 
-Output Format 
-Output 'True' if neither number is greater than zero, otherwise 'False'. 
-Sample Test Case 1 
-Input 
--5-3 
-Output 
-True 
-Sample Test Case 2 
-Input 
-Output 
-0-1 
-True
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    
+    if (!(num1 > 0) && !(num2 > 0)) {
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
+    
+    return 0;
+}
