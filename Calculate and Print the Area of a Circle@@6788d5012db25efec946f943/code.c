c40574cbde222d1d6ea094683c69bb6219749759
Dusttvin <kunal0519.becse24@chitkara.edu.in>
2025-02-04 11:27:59 +0530

    Solution to Calculate and Print the Area of a Circle

---
+++ Calculate and Print the Area of a Circle@@6788d5012db25efec946f943/code.c
@@ -0,0 +1,16 @@
+Calculate and Print the Area of a Circle 
+Write a program to take the radius of a circle as input and print its area using `printf`. Take value of nas 3.14. 
+Input Format 
+A single floating-point number representing the radius. 
+Output Format 
+Print the area of the circle. 
+Sample Test Case 1 
+Input 
+1 
+Output 
+Area: 3.14 
+Sample Test Case 2 
+Input 
+Output 
+2.5 
+Area: 19.62
